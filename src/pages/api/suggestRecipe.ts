bd8b77a93bc623e42b5f1400dee0300122063de2
AKULA SRI HARSHA SRI SAI HANUMAN <116995673+AHarshaNaidu@users.noreply.github.com>
2024-10-28 17:35:58 +0530

    Update suggestRecipe.ts

--- src/pages/api/suggestRecipe.ts
+++ src/pages/api/suggestRecipe.ts
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const Groq = require('groq-sdk');
-const groq = new Groq({ apiKey: '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', dangerouslyAllowBrowser: true });
+const groq = new Groq({ apiKey: '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', dangerouslyAllowBrowser: true });
 export async function suggestRecipe(ingredients: string[]) {
   const chatCompletion = await groq.chat.completions.create({
     "messages": [
